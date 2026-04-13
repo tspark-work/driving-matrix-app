5dfce30873c4a9a672b37ce82b1c80086175e957
Taeseong Park <39086628+tspark-work@users.noreply.github.com>
2026-04-13 10:18:12 +0900

    Update driving_map.py

--- driving_map.py
+++ driving_map.py
@@ -14,7 +14,6 @@ from scipy.ndimage import gaussian_filter
 import platform
 import plotly.graph_objects as go
 import plotly.express as px
-from score_driving import score_driving_style, ScoreConfig
 from matplotlib.colors import LogNorm
 from mpl_toolkits.mplot3d import Axes3D
 
